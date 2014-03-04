c13088e4b892a8d8d71ca9ff5777425175b593e5
apux <azarel.doroteo@logicalbricks.com>
2014-03-03 18:57:31 -0600

    Permite asociar tareas con empleados.

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -72,7 +72,7 @@ class ProyectosController < ApplicationController
     def proyecto_params
       params.require(:proyecto).permit(
         :nombre, :fecha_entrega, tareas_attributes: [
-          :id, :nombre, :prioridad, :_destroy
+          :id, :nombre, :prioridad, :_destroy, empleado_ids: []
         ]
       )
     end
